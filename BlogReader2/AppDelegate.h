05aa422ef744b73b57ab9ec1caa62ce8f549d876
epan <ericpan@gmail.com>
2013-01-09 15:58:58 -0800

    First commit
    empty project

---
+++ BlogReader2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BlogReader2
+//
+//  Created by Eric Pan on 1/9/13.
+//  Copyright (c) 2013 Eric Pan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BlogReader2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BlogReader2
+//
+//  Created by Eric Pan on 1/9/13.
+//  Copyright (c) 2013 Eric Pan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
